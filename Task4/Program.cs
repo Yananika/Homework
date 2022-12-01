d9ca1f7688d9bca7735e495cb58015e7d3cb934c
Yana <nikandrova0915@gmail.com>
2022-12-01 08:49:11 +0300

    Задача4

---
+++ Task4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int count = 2;
+while(count <= N)
+{
+    Console.Write($"{count}, ");
+    count = count + 2;
+}
