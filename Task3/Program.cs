fccf8c0154f2f6b6d643ac817b6425a6f7989102
Yana <nikandrova0915@gmail.com>
2022-12-01 08:23:20 +0300

    Задача 3

---
+++ Task3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число:");
+int number = int.Parse (Console.ReadLine());
+if (number % 2 ==0)
+{
+    Console.WriteLine("Это число чётное");
+}
+else Console.WriteLine("Это число нечётное");
